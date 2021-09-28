4a2fea643869a54551cc87238c2de307e683f3ec
Ondřej Čertík <ondrej@certik.us>
2021-09-28 13:33:54 -0600

    Declare undeclared variables, dble->real

--- scipy/stats/statlib/spearman.f
+++ scipy/stats/statlib/spearman.f
@@ -11,6 +11,7 @@ c
       integer, intent(in) :: n, is
       integer, intent(out) :: ifault
       integer :: l(6)
+      integer, parameter :: dp = kind(0.d0)
       double precision b, x, y, z, u
       double precision, parameter :: zero = 0.0d0, one=1.0d0,
      $    two=2.0d0, six=6.0d0
@@ -18,7 +19,7 @@ c
      $  c3 =0.1745d0, c4 =0.0758d0, c5 =0.1033d0, c6 =0.3932d0,
      $  c7 =0.0879d0, c8 =0.0151d0, c9 =0.0072d0, c10=0.0831d0,
      $  c11=0.0131d0, c12=0.00046d0
-      integer :: js
+      integer :: js, nfac, i
 c
 c        Test admissibility of arguments and initialize
 c
@@ -40,7 +41,7 @@ c
         nfac = nfac * i
         l(i) = i
     1 continue
-      prho = one / dble(nfac)
+      prho = one / real(nfac,dp)
       if (js .eq. n * (n * n -1) / 3) return
       ifr = 0
       do 5 m = 1,nfac
@@ -60,13 +61,13 @@ c
         n1 = n1 - 1
         if (m .ne. nfac) goto 3
     5 continue
-      prho = dble(ifr) / dble(nfac)
+      prho = real(ifr,dp) / real(nfac,dp)
       return
 c
 c        Evaluation by Edgeworth series expansion
 c
-    6 b = one / dble(n)
-      x = (six * (dble(js) - one) * b / (one / (b * b) -one) -
+    6 b = one / real(n,dp)
+      x = (six * (real(js,dp) - one) * b / (one / (b * b) -one) -
      $  one) * sqrt(one / b - one)
       y = x * x
       u = x * b * (c1 + b * (c2 + c3 * b) + y * (-c4
