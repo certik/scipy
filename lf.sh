b4f5090d771d00b71ec8ef633e2d288d15e7781e
HarshitaKalani <kalani.1@iitj.ac.in>
2023-11-01 23:47:29 +0530

    script to run

---
+++ lf.sh
@@ -0,0 +1,15 @@
+git fetch ondrej
+git submodule update --init
+mkdir lfortran-build/
+cd lfortran-build/
+LIBRARY_PATH="/home/harshita/Desktop/lfortran/src/runtime/"
+FC=lfortran cmake \
+  -DCMAKE_Fortran_FLAGS=--verbose \
+  -DLFORTRAN_RUNTIME_LIBRARY_PATH=$LIBRARY_PATH \
+  ..
+make install
+cp /home/harshita/Desktop/lfortran/src/runtime/liblfortran_runtime.*  $CONDA_PREFIX/lib
+cd ../
+python dev.py build
+python dev.py test -t scipy.sparse -v
+
